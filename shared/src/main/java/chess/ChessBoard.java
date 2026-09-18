6c931c4d6794fdd0ccdf4d4f07ab723aa73c3bf1
0thedarkweb0 <crazykids438@gmail.com>
2026-09-18 12:51:26 -0600

    ALL tests passing but chessboard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -18,7 +21,7 @@ public class ChessBoard {
      * @param position where to add the piece to
      * @param piece    the piece to add
      */
-    public void addPiece(ChessPosition position, ChessPiece piece) {
+    public void addPiece(ChessPosition position, ChessPiece piece){
         squares[position.getRow()][position.getColumn()] = piece;
     }
 
@@ -37,7 +40,21 @@ public class ChessBoard {
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
-    public void resetBoard() {
+    public void resetBoard(){
         throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
     }
 }
